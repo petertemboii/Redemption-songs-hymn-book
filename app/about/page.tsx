3a0dd94c1bb5ba3eed18ca0c4f80b1a563463778
petertemboii <tembopeter90@gmail.com>
2024-05-15 01:26:07 +0200

    Fix apostrophe issue.

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,19 +13,21 @@ export default function page() {
 
       <div className="py-6">
         <h1 className="font-medium text-center text-4xl">Get in Touch</h1>
-        <p className="text-center">We'd love to hear from you! </p>
+        <p className="text-center">We would love to hear from you! </p>
         <p className="text-center">
           Whether you have corrections, suggestions, or just want to say hello,
-          feel free to reach out to the contributers:    
+          feel free to reach out to the contributers:
         </p>
         <p className="text-center">Name: Peter Tembo</p>
         <p className="text-center">Email: tembopeter90@gmail.com</p>
         <p className="text-center">Phone: +260 (97) 1598846</p>
         <p className="text-center">Location: Lusaka, Zambia</p>
-        
       </div>
-      <div><h1 className="text-center">Thank you for being a part
-          of the Redemption Songs PWA community!</h1></div>
+      <div>
+        <h1 className="text-center">
+          Thank you for being a part of the Redemption Songs PWA community!
+        </h1>
+      </div>
     </div>
   );
 }
