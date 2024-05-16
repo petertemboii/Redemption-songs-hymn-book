b4c1e15e71d3d24e1a070b77da6984b920be4bf7
petertemboii <tembopeter90@gmail.com>
2024-05-16 09:39:14 +0200

    Add the song counter in the footer and more songs to the array.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,19 +1,25 @@
 import Link from "next/link";
 import React from "react";
+import songs from "@/constants/songs";
 
 const Footer = () => {
   const currentDate = new Date().getFullYear();
 
   return (
-    <footer className="flex justify-center  text-center py-4 fixed bottom-0 w-full bg-gray-200 py-6">
-      <div>
-        <p>Copyright {currentDate}</p>
+    <footer className="justify-center  text-center fixed bottom-0 w-full bg-gray-200 py-6">
+      <div className="flex flex-row justify-center">
+        <div>
+          <p>Copyright {currentDate}</p>
+        </div>
+        <nav className="px-2">
+          <Link href="/about">
+            <p>About</p>
+          </Link>
+        </nav>
       </div>
-      <nav className="px-2">
-        <Link href="/about">
-          <p>About</p>
-        </Link>
-      </nav>
+      <div>
+        <p className="pt-3">(We have currently added {songs[songs.length - 1].number} of 945 songs.)</p>
+      </div>
     </footer>
   );
 };
